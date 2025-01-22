710cb145787301eaa491c3ee993f4de3d2028348
shubham-mehta-002 <mehta.shubham002@gmail.com>
2025-01-22 10:57:01 +0530

    add port

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ const app = express();
 connectDB();
   
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL
+}));
 
 // Increase payload size limit to 50mb
 app.use(express.json({ limit: '50mb' }));
@@ -32,7 +34,7 @@ const templateRoutes = require('./routers/template.router');
 app.use("/api/auth", authRoutes);
 app.use("/api/templates", templateRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);

f4a89e94bea72aa6b42713a4a20a4663e8b3b9b5
shubham-mehta-002 <mehta.shubham002@gmail.com>
2025-01-22 11:11:40 +0530

    add deployed link

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  // baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://emailbuilder-1-9gi9.onrender.com/api'
 });
 
 // Add token to requests

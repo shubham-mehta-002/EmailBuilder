5e8bbfd672e3be62f5897fbae36e9336be5445e3
Shubham Mehta <134730928+shubham-mehta-002@users.noreply.github.com>
2025-01-22 19:03:57 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'http://localhost:8000/api'
-  baseURL: 'https://emailbuilder-1-9gi9.onrender.com/api/'
+  baseURL: 'https://emailbuilder-1-9gi9.onrender.com/api'
 });
 
 // Add token to requests
